0ab5d4de5f15eca1e7cb38e4404d74d0b768ba51
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2016-02-12 00:20:42 +0300

    [#]  master Work stage 2016-02-12 00:20:42  M src/router/api.php

--- src/router/api.php
+++ src/router/api.php
@@ -6,7 +6,20 @@ $app->group('/api/v1', function () use ($app) {
     $app->group('/volume', function () use ($app) {
         $app->map('/down', function () use ($app) {
             $app->response->headers->set('Content-Type', 'application/json');
-            if ($status = system('amixer -c 0 -q set Master 3%- unmute')) {
+            $status = system('amixer -c 0 -q set Master 3%- unmute');
+            if ($status !== false) {
+                $status = 200;
+            } else {
+                $status = 500;
+            }
+            $app->halt(200, (new ApiResponse(array(), array(compact('status')))));
+        })
+            ->via('GET');
+
+        $app->map('/up', function () use ($app) {
+            $app->response->headers->set('Content-Type', 'application/json');
+            $status = system('amixer -c 0 -q set Master 3%+ unmute');
+            if ($status !== false) {
                 $status = 200;
             } else {
                 $status = 500;
@@ -15,17 +28,20 @@ $app->group('/api/v1', function () use ($app) {
         })
             ->via('GET');
     });
-});
 
-$app->group('/api/v1', function () use ($app) {
-    $app->group('/volume', function () use ($app) {
-        $app->map('/up', function () use ($app) {
+    $app->group('/key', function () use ($app) {
+        $app->map('/space', function () use ($app) {
             $app->response->headers->set('Content-Type', 'application/json');
-            if ($status = system('amixer -c 0 -q set Master 3%+ unmute')) {
-                $status = 200;
+            $XAUTHORITY = 'export XAUTHORITY=/home/user/.Xauthority; ';
+            $status = system(/*$XAUTHORITY . */'export DISPLAY=:0.0; xdotool key space');
+            if ($status !== false) {
+                //$status = 200;
             } else {
-                $status = 500;
+                //$status = 500;
             }
+                echo "<pre>";var_dump(
+                $status
+                );die;
             $app->halt(200, (new ApiResponse(array(), array(compact('status')))));
         })
             ->via('GET');
